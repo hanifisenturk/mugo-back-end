015ba41b58d30660b13959c0df2b628114e71324
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-10 18:15:54 +0300

    The scheme of marquee has been created.

---
+++ schemas/marquee.js
@@ -0,0 +1,12 @@
+export default {
+  name: "marquee",
+  title: "Marquee Content",
+  type: "document",
+  fields: [
+    {
+      name: "content",
+      title: "Content",
+      type: "string",
+    },
+  ],
+};
